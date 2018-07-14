406d760295452ce1141ae26150c9fdce03da60d2
Daniel Oaks <daniel@danieloaks.net>
2018-07-15 00:46:35 +1000

    Write EventQueue and clear bar values

--- js/wrpg.js
+++ js/wrpg.js
@@ -1,20 +1,30 @@
 function GuiManager() {
     var g = {
         init() {
+            this.rBars = document.querySelectorAll('#main .right-pane .main-info .bar')
+            this.rBarValues = document.querySelectorAll('#main .right-pane .main-info .bar .value')
+            this.rCharacterName = document.querySelector('#main .right-pane .main-info .character-name')
+            this.rContent = document.querySelector('#main .center-pane .content')
+            this.rControlButtons = document.querySelectorAll('#main .center-pane > .buttons .kbutton')
+            this.rDay = document.querySelector('#main .left-pane .timeholder .day .value')
             this.rPlaceName = document.querySelector('#main .left-pane .main-info .place .description')
             this.rRegionName = document.querySelector('#main .left-pane .main-info .region-name')
-            this.rControlButtons = document.querySelectorAll('#main .center-pane > .buttons .kbutton')
-            this.content = document.querySelector('#main .center-pane .content')
-            this.day = document.querySelector('#main .left-pane .timeholder .day .value')
-            this.time = document.querySelector('#main .left-pane .timeholder .time .value')
+            this.rTime = document.querySelector('#main .left-pane .timeholder .time .value')
         },
 
         blankScreen() {
+            this.rCharacterName.innerHTML = '&nbsp;'
+            this.rContent.innerHTML = '&nbsp;'
+            this.rDay.innerText = '0'
             this.rPlaceName.innerText = ''
             this.rRegionName.innerHTML = '&nbsp;'
-            this.content.innerHTML = '&nbsp;'
-            this.day.innerText = '0'
-            this.time.innerText = '00:00'
+            this.rTime.innerText = '00:00'
+            for (var i = 0, len = this.rBars.length; i < len; i++) {
+                this.rBars[i].style = 'background-position-x: calc(100% - 100% * (0 / 100));'
+            }
+            for (var i = 0, len = this.rBarValues.length; i < len; i++) {
+                this.rBarValues[i].innerText = '0'
+            }
             for (var i = 0, len = this.rControlButtons.length; i < len; i++) {
                 this.rControlButtons[i].classList.remove('active')
                 this.rControlButtons[i].children[0].innerHTML = '&nbsp;'
@@ -25,8 +35,10 @@ function GuiManager() {
     return g
 }
 
-function ButtonManager() {
+function ButtonManager(eventQueue) {
     var b = {
+        eventQueue: eventQueue,
+
         keyPresses: {},
 
         init() {
@@ -48,25 +60,104 @@ function ButtonManager() {
 
                 var btn = this.keyPresses[event.key]
                 if (btn != null) {
-                    console.log('got a keyboard press', btn)
+                    // console.log('got a keyboard press', btn)
+                    this.eventQueue.dispatch('btn ' + btn)
                 }
             })
         },
 
         pressHandler(event) {
             event.preventDefault()
-            console.log('got a button press', event.currentTarget.dataset.btn)
+            // console.log('got a button press', event.currentTarget.dataset.btn)
+            this.eventQueue.dispatch('btn ' + event.currentTarget.dataset.btn)
         },
     }
 
     return b
 }
 
+// EventQueue turns a series of async incoming events into a sync stream of handler calls.
+// only works if all handlers are sync (don't return promises or w/e)
+function EventQueue() {
+    var e = {
+        workingOnHandlers: false, // manually-controlled mutex
+        pendingHandlers: [],
+        handlers: {},
+
+        workingOnEvents: false, //manually-controlled mutex
+        pendingEvents: [],
+
+        addHandler(name, handler) {
+            this.pendingHandlers.push({
+                'name': name,
+                'handler': handler
+            })
+
+            if (!this.workingOnHandlers) {
+                this.workingOnHandlers = true
+
+                while (true) {
+                    var newHandler = this.pendingHandlers.shift()
+
+                    if (newHandler === undefined) {
+                        break
+                    }
+
+                    var currentHandlers = this.handlers[name]
+
+                    if (currentHandlers == null) {
+                        currentHandlers = []
+                    }
+
+                    currentHandlers.push(handler)
+
+                    this.handlers[name] = currentHandlers
+                }
+
+                this.workingOnHandlers = false
+            }
+        },
+
+        dispatch(name) {
+            // console.log('dispatching event', name)
+            this.pendingEvents.push(name)
+
+            if (!this.workingOnEvents) {
+                this.workingOnEvents = true
+
+                while (true) {
+                    var newEvent = this.pendingEvents.shift()
+                    if (newEvent === undefined) {
+                        break
+                    }
+
+                    var handlers = this.handlers[newEvent]
+                    if (handlers == null) {
+                        continue
+                    }
+
+                    for (var i = 0, len = handlers.length; i < len; i++) {
+                        // console.log('calling handler', handlers[i], 'with event', newEvent)
+                        handlers[i](newEvent)
+                    }
+                }
+
+                this.workingOnEvents = false
+            }
+        },
+    }
+
+    return e
+}
+
 function Engine() {
+    var eq = EventQueue()
+
     var e = {
         state: 'start',
 
-        Buttons: ButtonManager(),
+        Buttons: ButtonManager(eq),
+        Events: eq,
         Gui: GuiManager(),
 
         init() {
